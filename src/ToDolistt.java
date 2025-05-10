fefd854dec2d9af575cd0b69c2acb24293c4a8a9
Abdul Wali Dal <148618788+abdulwalidal@users.noreply.github.com>
2025-05-10 07:05:11 +0500

    added comments for clarity

--- src/ToDolistt.java
+++ src/ToDolistt.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 public class ToDolistt {
     Scanner sc = new Scanner(System.in);
 
-    static String[] tasklist = new String[5];
-    static int index = 0;
-    int[] taskno;
+    static String[] tasklist = new String[5]; // String Array for storing tasknames with the size of 5
+    static int index = 0; // for tracking the tasks in arrays
+    int[] taskno; // have not used this
 
-    public static void display() {
+    public static void display() { // options i will be having in my list
         Scanner sc = new Scanner(System.in);
 
         System.out.println("\nWelcome to *** To Do List***\n");
@@ -40,53 +40,60 @@ public class ToDolistt {
         }
     }
 
-    public static void addTask() {
+    public static void addTask() {  // adding a task in my todolist
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Task name");
         String task = sc.nextLine();
 
-        if (index < tasklist.length) {
-            tasklist[index] = task;
-            index++;
+        if (index < tasklist.length) { // checking if tasklist is not full
+            tasklist[index] = task; // this would add task at a specific available position
+            index++; // that available position is not free anymore and goes by 1
             System.out.println("Task is added Successfully");
         }
-        ToDolistt.display();
+        ToDolistt.display(); // it would again put choices of todolist
     }
 
-    public static void ViewTask() {
-        if (index == 0) {
+    public static void ViewTask() { // viewing all the tasks
+        if (index == 0) {  // checking if my list is empty or not
             System.out.println("Your ToDoList is empty");
-            display();
+            display(); // if it is empty this will again give choices of todolist
         } else {
             System.out.println("Your Tasks : ");
-            for (int i = 0; i < index; i++) {
-                System.out.println(tasklist[i]);
+            for (int i = 0; i < index; i++) { // this would go through from index 0
+                System.out.println(tasklist[i]); // it would print each task
             }
-            display();
+            display(); // choices would be given agan after viewing
         }
     }
 
-    public static void deleteTask() {
+    public static void deleteTask() { // deleting a task from the todolist
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the task number you want to delete");
-        int deletetaskno = sc.nextInt();
+        int deletetaskno = sc.nextInt(); // which task you want to delete
         if (deletetaskno > 0 && deletetaskno < index && tasklist[deletetaskno] != null) {
-            tasklist[deletetaskno] = null;
-            index--;
+            tasklist[deletetaskno] = null; // task in deleted
+            index--; // would decrease the index too
         } else {
             System.out.println("ToDoList is empty or invalid task number.");
         }
 
-        for (int i = deletetaskno; i < index; i++) {
-            tasklist[deletetaskno] = tasklist[i + 1];
+        for (int i = deletetaskno; i < index; i++) { // it starts from the deletetaskno so we can move
+            tasklist[deletetaskno] = tasklist[i + 1]; // moving the deleted task forward
         }
 
-        tasklist[index] = null;
+        tasklist[index] = null; // deleting the duplicate
         System.out.println("Task is deleted");
-        display();
+        display(); // choices will be given again
     }
+
+
+
+
 
     public static void main(String[] args) {
         ToDolistt.display();
     }
 }
+
+
+
